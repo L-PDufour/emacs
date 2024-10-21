c60f1586704c2ae450111aef218e25b27a039ffd
L-PDufour <leonpierre.dufour@gmail.com>
2024-10-21 11:52:19 -0400

    org

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   description = "Custom Emacs build with Nix-managed packages";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -10,71 +10,30 @@
     {
       self,
       nixpkgs,
+      emacs-overlay,
       flake-utils,
+      ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        overlays = [ emacs-overlay.overlay ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        tangledConfig = ./emacs-config.org; # Ensure your Org file is in the same directory
 
-        myEmacsPackages =
-          epkgs: with epkgs; [
-            vterm
-            magit
-            org
-            which-key
-            use-package
-            command-log-mode
-            evil
-            evil-collection
-            evil-escape
-            ligature
-            projectile
-            company
-            catppuccin-theme
-            vertico
-            marginalia
-            orderless
-            consult
-            embark
-            embark-consult
-            wgrep
+        emacsWithPackages = pkgs.emacsWithPackagesFromUsePackage {
+          config = tangledConfig;
+          defaultInitFile = true;
+          package = pkgs.emacs-unstable;
+          alwaysEnsure = true;
+          alwaysTangle = true;
+          extraEmacsPackages = epkgs: [
+            epkgs.vterm
           ];
-
-        myEmacs = (pkgs.emacsPackagesFor pkgs.emacs).emacsWithPackages myEmacsPackages;
-
-        emacsConfig = pkgs.runCommand "emacs-config" { } ''
-          mkdir -p $out/share/emacs/site-lisp/user
-          cp ${./init.el} $out/share/emacs/site-lisp/user/init.el
-          cp ${./early-init.el} $out/share/emacs/site-lisp/user/early-init.el
-          ${pkgs.lib.optionalString (builtins.pathExists ./extras) "cp -r ${./extras} $out/share/emacs/site-lisp/user/extras"}
-        '';
-
+        };
       in
       {
-        packages = {
-          default = pkgs.symlinkJoin {
-            name = "custom-emacs";
-            paths = [
-              myEmacs
-              emacsConfig
-            ];
-            buildInputs = [ pkgs.makeWrapper ];
-            postBuild = ''
-              wrapProgram $out/bin/emacs \
-                --add-flags "-Q" \
-                --add-flags "-l $out/share/emacs/site-lisp/user/early-init.el" \
-                --add-flags "-l $out/share/emacs/site-lisp/user/init.el" \
-                --prefix PATH : ${
-                  pkgs.lib.makeBinPath [
-                    pkgs.ripgrep
-                    pkgs.fd
-                  ]
-                }
-            '';
-          };
-        };
-
+        packages.default = emacsWithPackages;
         apps.default = {
           type = "app";
           program = "${self.packages.${system}.default}/bin/emacs";
@@ -83,7 +42,6 @@
         devShells.default = pkgs.mkShell {
           buildInputs = [
             self.packages.${system}.default
-            pkgs.just
           ];
         };
       }
